294abc56f04c0d29f0ed2886224ac0b2e95167bb
0scar-M <133919624+0scar-M@users.noreply.github.com>
2023-07-21 10:37:13 +1200

    Update 2.3 Database Assesment.py
    Wrote the add_participation function. Had to make some changes to valid_input so that it would work when inputting numeric values where you don't have a values list.

--- 2.3 Database Assesment.py
+++ 2.3 Database Assesment.py
@@ -1,13 +1,19 @@
 import sqlite3 as sql
 import os
 
-def valid_input(prompt, error_prompt, type_, values):
+def valid_input(prompt, error_prompt, type_, *values):
+    """
+    Gets input from the user and validates it.
+    error_prompt will be shown to the user if they input an incorrect value
+    type_ is the type of the input. If it is str then the program will check that input_ is in values.
+    """
     while True:
         try:
             input_ = input(prompt)
             if input_ == "exit": exit()
             input_ = type_(input_)
-            if input_ not in values: raise
+            if type_ == str:
+                if input_ not in values[0]: raise
             break
         except Exception:
             print(error_prompt)
@@ -17,7 +23,19 @@ def results():
     pass
 
 def add_participation():
-    pass
+    """
+    Adds participations to the database for certain events and groups.
+    """
+    global events, groups
+    events = [x[0] for x in cursor.execute("SELECT name FROM events ORDER BY id;").fetchall()]
+    groups = [x[0] for x in cursor.execute("SELECT house||\' \'||gender FROM groups ORDER BY id;").fetchall()]
+    
+    event_id = events.index(valid_input("Enter the event that you want to add a participation for: ", f"Make sure that you have entered one of these events: {', '.join(events).title()}", str, events))+1
+    group_id = groups.index(valid_input("Enter the group that you want to add a participation for: ", f"Make sure that you have entered one of these groups: {', '.join(groups).title()}", str, groups))+1
+    avg_score = valid_input(f"Enter the average score for {groups[group_id-1]}: ", "Please make sure that you entered a valid number.", float)
+    
+    cursor.execute("INSERT INTO participations VALUES (?, ?, ?);", (event_id, group_id, avg_score))
+    conn.commit()
 
 def edit_participation():
     pass
